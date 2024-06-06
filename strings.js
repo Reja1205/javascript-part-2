780e0dd876f9e25df5f30000a3d24c726bc8b160
Rejaur rahman <rejaur1990@gmail.com>
2024-06-06 19:42:53 -0400

    JS String

---
+++ strings.js
@@ -0,0 +1,25 @@
+//let firstName='Rejaur Rahman';
+//Length property
+//let length=firstName.length;
+//console.log(firstName.length);
+
+//console.log(firstName.toUpperCase());
+//console.log(firstName.toLowerCase());
+//console.log(firstName.charAt(5));
+
+//console.log(firstName.slice(7,13));
+
+//console.log(firstName.split(" "));
+//console.log(firstName.substring(0,6));
+
+//console.log(firstName.replace("Rejaur", "Rejawana"));
+
+//console.log(firstName.indexOf("j"));
+
+//String Object
+let country =new String("Bangladesh");
+//console.log(country);
+
+let firstName="Rejaur";
+let lastName="Rahman";
+console.log(firstName.concat(" ",lastName));
