b59f99c3780aa2d32915748c22f0a963e0e1201a
Rejaur rahman <rejaur1990@gmail.com>
2024-06-11 16:51:20 -0400

    Arras

---
+++ arrays.js
@@ -0,0 +1,37 @@
+       const cars=["BMW", "Toyota", "Nissan","Ford"];
+       let position=cars.indexOf("Ford")+1;
+         console.log(position);
+
+      //cars.push("Kia");
+     // console.log(cars);
+    //  console.log(cars.pop());
+    //  console.log(cars);
+        //console.log(cars);
+        // let car=cars[1];
+        // console.log(car);
+        //cars[0]="Honda";
+        //console.log(cars);
+       // console.log(cars.length);
+       //let car=cars[cars.length-1];
+       //console.log(car);
+       //console.log(cars.push("Kia"));
+
+    //    console.log(cars.shift());
+    //    console.log(cars);
+    // cars.unshift("Nikola");
+    // console.log(cars);
+    // delete cars[0];
+    // console.log(cars);
+    
+    
+
+
+        // const cars= [];
+        // cars[0]="BMW";
+        // cars[1]="Ford";
+        // cars[2]="Nissan";
+        // console.log(cars);
+
+
+
+
