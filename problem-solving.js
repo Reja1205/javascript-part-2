999a88bff63609b881e85dd0011592de2980dfbc
Rejaur rahman <rejaur1990@gmail.com>
2024-06-12 15:03:59 -0400

    Problem solving

---
+++ problem-solving.js
@@ -0,0 +1,36 @@
+//Convert a string to uppercase with a function
+// let myStr="My name is rejaur rahman";
+
+// function convertToUpperCase(str){
+//     return str.toUpperCase();
+// }
+// let convertedStr=convertToUpperCase(myStr);
+// console.log(convertedStr);
+
+//Find the total lenth of a string, using a function
+// let myStr="Rejaur Rahman";
+// function totalLength(str){
+//     return str.length;
+// }
+// let strLength=totalLength(myStr);
+// console.log(strLength);
+
+//Write a program that print from 10 to 1
+// for (let i=10;i>=1;i--){
+//     console.log(i);
+// }
+
+//write a program that will print the even number from 1 to 100
+// for (let i =1;i<=100;i++){
+//     if(i%2===0){
+//         console.log(i);
+//     }
+// }
+
+//write a program that sum an array elements
+const arrayElements=[2,5,7,8];
+let sum=0;
+for(let i=0;i<arrayElements.length;i++){
+    sum=sum+arrayElements[i];
+    console.log(sum);
+}
